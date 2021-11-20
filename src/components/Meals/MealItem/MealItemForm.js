3547d88f71b33f770e2cc359677810d77ad653ab
YBogdan RADOI <radoi.office@gmail.com>
2021-11-20 22:44:59 +0100

    Adds refs and forwardRef

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,29 @@
+import { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  
+  // To pass a ref to a custom element we need to user forwardRef in the custom element
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+    if (enteredAmount.trim().length === 0 || enteredAmountNumber < 0 || enteredAmountNumber > 5) {
+      setAmountIsValid(false);
+      return;
+    }
+    // pass entered amount to MealItem where all the info is available to update context (add to items)
+    props.addToCart(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref = {amountInputRef}
         label="Amount"
         input={{
           id: "amount_" + props.id,
@@ -16,6 +35,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount (number between 1-5).</p>}
     </form>
   );
 };
